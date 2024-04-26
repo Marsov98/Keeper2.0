581d0e3bdb9cf5610c78f8bdf722fce20b081388
Максим <m.s.kalinichenko@copp26.ru>
2024-04-26 17:54:34 +0300

    Добавил Android приложение охраны. Дай Бог заведется на андроиде

---
+++ Security Android/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Security_Android
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
