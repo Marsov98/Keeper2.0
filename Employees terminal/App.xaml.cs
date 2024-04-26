e6c7fc6ba19bcbc66dfe9d674aef3660f9d8be33
F0Rz <76008321+fr0z1@users.noreply.github.com>
2024-04-26 11:30:20 +0300

    Создал проект под сотрудника подразделения
    Там пусто пока, авторизация с терминала охраны и домашняя страница взята там же, добавил на обоих домашних пару отступов(теперь выглядит ровнее)

---
+++ Employees terminal/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Employees_terminal
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
---
+++ Employees terminal/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Employees_terminal
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
